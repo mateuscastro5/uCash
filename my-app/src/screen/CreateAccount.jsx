c068e2727483a76ef9d389503ab382ff17894ac4
Tue <mateus.castro2011@gmail.com>
2024-07-30 14:46:13 -0300

    Update Spacing and Keyboard avoiding

--- my-app/src/screen/CreateAccount.jsx
+++ my-app/src/screen/CreateAccount.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -8,35 +8,51 @@ const CreateAccount = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.headingText}>Ai sim!</Text>
-        <Text style={styles.headingText}>Vamos criar a sua conta</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.keyboardAvoidingView} behavior="padding">
+        <View style={styles.textContainer}>
+          <Text style={styles.headingText}>Ai sim!</Text>
+          <Text style={styles.headingText}>Vamos criar a sua conta</Text>
+        </View>
 
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Empresa"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="CPF"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Crie sua senha"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Repita sua senha"
-        />
-      </View>
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Ionicons name="briefcase" size={24} color="#3A3A3A" style={styles.inputIcon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Empresa"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Ionicons name="person" size={24} color="#3A3A3A" style={styles.inputIcon} />
+            <TextInput
+              style={styles.input}
+              placeholder="CPF"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Ionicons name="lock-closed" size={24} color="#3A3A3A" style={styles.inputIcon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Crie sua senha"
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Ionicons name="lock-closed" size={24} color="#3A3A3A" style={styles.inputIcon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Repita sua senha"
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.createAccountButton}>
-          <Text style={styles.createAccountButtonText}>Criar Conta</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.createAccountButton}>
+            <Text style={styles.createAccountButtonText}>Criar Conta</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -46,6 +62,10 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#F3F3F3',
+  },
+  keyboardAvoidingView: {
+    justifyContent: 'center',
+    padding: 20,
   },
   headingText: {
     fontSize: 38,
@@ -58,17 +78,27 @@ const styles = StyleSheet.create({
     marginTop: 20,
     flexDirection: 'column',
   },
-  input: {
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     borderWidth: 1,
     maxWidth: 300,
     borderRadius: 20,
-    paddingHorizontal: 30,
+    paddingHorizontal: 10,
     height: 40,
     marginBottom: 10,
     marginVertical: 20,
     alignSelf: 'stretch',
     marginLeft: 40,
     marginRight: 40,
+  },
+  inputIcon: {
+    marginLeft: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    paddingHorizontal: 10,
   },
   buttonContainer: {
     flexDirection: 'column',
@@ -91,24 +121,6 @@ const styles = StyleSheet.create({
     width: 200, // largura igual para os botões
   },
   createAccountButtonText: {
-    fontSize: 18,
-    color: '#3A3A3A',
-  },
-  backButton: {
-    alignItems: 'center',
-    backgroundColor: '#31FF9C', // Verde
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 20,
-    marginBottom: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    width: 200, // largura igual para os botões
-  },
-  backButtonText: {
     fontSize: 18,
     color: '#3A3A3A',
   },
--- my-app/src/screen/HomeScreen.jsx
+++ my-app/src/screen/HomeScreen.jsx
@@ -1,69 +1,78 @@
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Colors } from '../../constants/Colors'
-import { DarkTheme } from '@react-navigation/native'
-import { useFonts } from 'expo-font'
-import { useNavigation } from '@react-navigation/native'
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
-    const navigation = useNavigation()
+  const navigation = useNavigation();
+
   return (
-
     <SafeAreaView style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.headingText}>Ola,</Text>
-        <Text style={styles.headingText}>bem-vindo ao seu Space</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.keyboardAvoidingView} behavior="padding">
+        <View style={styles.textContainer}>
+          <Text style={styles.headingText}>Bem-vindo!</Text>
+          <Text style={styles.headingText}>Faça login para continuar</Text>
+        </View>
 
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="CPF"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Senha"
-        />
-        <TouchableOpacity onPress={() => handleForgotPassword()}>
-          <Text style={styles.forgotPasswordText}>Esqueceu sua senha?</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Ionicons name="person" size={24} color="#3A3A3A" style={styles.inputIcon} />
+            <TextInput
+              style={styles.input}
+              placeholder="CPF"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Ionicons name="lock-closed" size={24} color="#3A3A3A" style={styles.inputIcon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Senha"
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.createAccountButton} onPress={() => navigation.navigate('CreateAccount')}>
-          <Text style={styles.createAccountButtonText}>Criar Conta</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>Entrar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.createAccountButton, { marginTop: 20 }]} onPress={() => navigation.navigate('CreateAccount')}>
+            <Text style={styles.createAccountButtonText}>Criar Conta</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
-  )
-}
-
-export default HomeScreen
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    backgroundColor: '#F3F3F3',
   },
-  headingText: { 
+  keyboardAvoidingView: {
+    justifyContent: 'center',
+    padding: 20,
+  },
+  headingText: {
     fontSize: 38,
   },
   textContainer: {
     margin: 40,
+    marginBottom: 20,
   },
   formContainer: {
     marginTop: 20,
     flexDirection: 'column',
   },
-  input: {
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     borderWidth: 1,
     maxWidth: 300,
     borderRadius: 20,
-    paddingHorizontal: 30, 
+    paddingHorizontal: 10,
     height: 40,
     marginBottom: 10,
     marginVertical: 20,
@@ -71,22 +80,23 @@ const styles = StyleSheet.create({
     marginLeft: 40,
     marginRight: 40,
   },
-  forgotPasswordText: {
-    fontSize: 16,
-    color: '#373737', 
-    textDecorationLine: 'none', 
-    alignSelf: 'center', 
-    marginBottom: 20,
+  inputIcon: {
+    marginLeft: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    paddingHorizontal: 10,
   },
   buttonContainer: {
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 20,
+    marginTop: 40,
   },
   loginButton: {
     alignItems: 'center',
-    backgroundColor: '#31FF9C', //Verde
+    backgroundColor: '#31FF9C', // Verde
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 20,
@@ -100,16 +110,15 @@ const styles = StyleSheet.create({
   },
   loginButtonText: {
     fontSize: 18,
-    color: '#373737', //Preto
+    color: '#3A3A3A',
   },
   createAccountButton: {
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#31FF9C', // Verde
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 20,
-    borderColor: '#31FF9C',
-    borderWidth: 1,
+    marginBottom: 10,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
@@ -119,6 +128,8 @@ const styles = StyleSheet.create({
   },
   createAccountButtonText: {
     fontSize: 18,
-    color: '#373737',
+    color: '#3A3A3A',
   },
-})
+});
+
+export default HomeScreen;
