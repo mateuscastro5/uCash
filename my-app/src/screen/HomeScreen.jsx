a4905d167b324d62ce82aad654d08b46ba73707f
Tue <mateus.castro2011@gmail.com>
2024-08-01 21:24:56 -0300

    Comment removal

--- my-app/src/screen/HomeScreen.jsx
+++ my-app/src/screen/HomeScreen.jsx
@@ -111,7 +111,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    width: 200, // largura igual para os botões
+    width: 200, 
   },
   loginButtonText: {
     fontSize: 18,
@@ -130,7 +130,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    width: 200, // largura igual para os botões
+    width: 200, 
   },
   createAccountButtonText: {
     fontSize: 18,
