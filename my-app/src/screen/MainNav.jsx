85ca165fd30c516e74586d00ae89adbd73e2fae4
Tue <mateus.castro2011@gmail.com>
2024-08-08 15:50:00 -0300

    Create MainNav.jsx

---
+++ my-app/src/screen/MainNav.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+
+
+const MainNav = () => {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>Aaa</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = stylesheet.create({
+    container: {
+})
+
+export default MainNav
